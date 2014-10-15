d97e30684b317ba6917c5380e16e9061da395830
Sam <saed_haddad@yahoo.com>
2014-10-14 18:12:28 -0700

    EDA project1

---
+++ plot1.R
@@ -0,0 +1,5 @@
+data<-read.table("household_power_consumption.txt",col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),sep = ";", skip = 66637, nrows = 2880,colClasses = "character")
+par(bg = "white")
+hist(as.numeric(data$Global_active_power),col = "red",xlab = "Global Active Power (kilowatts)",main = "Global Active Power")
+dev.copy(png, file = "plot1.png")
+dev.off()
